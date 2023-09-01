7e747f87eab97faf02a8a188d4e16915a618a5d5
kegadev <kegadev@outlook.com>
2023-09-01 15:41:38 -0500

    feature: add class Utils

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -2,21 +2,89 @@ library utils;
 
 import 'dart:math';
 
+import 'package:flutter/material.dart';
+
+import 'package:circulito/circulito.dart';
+
 part 'direction.dart';
 part 'start_point.dart';
 part 'section_value_type.dart';
 part 'circulito_stroke_cap.dart';
 
-/// Returns the start angle to draw the wheel from the [startPoint]
-double getStartAngle(StartPoint startPoint) {
-  switch (startPoint) {
-    case StartPoint.top:
-      return -pi / 2;
-    case StartPoint.bottom:
-      return pi / 2;
-    case StartPoint.left:
-      return pi;
-    case StartPoint.right:
-      return 0;
+/// Utility class.
+abstract class Utils {
+  /// Returns the start angle to draw the wheel from the [startPoint]
+  static double getStartAngle(StartPoint startPoint) {
+    switch (startPoint) {
+      case StartPoint.top:
+        return pi * 1.5;
+      // return -pi / 2;
+      case StartPoint.bottom:
+        return pi / 2;
+      case StartPoint.left:
+        return pi;
+      case StartPoint.right:
+        return 0;
+    }
+  }
+
+  /// Returns the total (sum) value of all sections.
+  ///
+  /// Must be used when types are [SectionValueType.amount].
+  static double getSectionsTotalValue(sections) {
+    return sections.fold(0.0, (sum, section) => sum + section.value);
+  }
+
+  /// Returns the distance from the center of the wheel to the [hoverPosition].
+  static double getHoverDistanceFromCenter(
+      Offset hoverPosition, Offset centerOffset) {
+    return sqrt(pow(hoverPosition.dx - centerOffset.dx, 2) +
+        pow(hoverPosition.dy - centerOffset.dy, 2));
+  }
+
+  /// Returns the angle in radians between the [tapPosition] and the center of
+  /// the wheel.
+  static double calculateHoverAngle(Offset tapPosition, Offset centerOffset,
+      double radius, CirculitoDirection direction, StartPoint startPoint) {
+    tapPosition - centerOffset;
+    final dx = tapPosition.dx - centerOffset.dx;
+    final dy = tapPosition.dy - centerOffset.dy;
+
+    var radians = atan2(dy, dx);
+    if (direction == CirculitoDirection.counterClockwise) radians = -radians;
+
+    radians = truncateRadiansAngle(radians);
+    radians -= getStartAngle(startPoint);
+    return truncateRadiansAngle(radians);
+  }
+
+  /// Keep the angle in radians between 0 and 2 * pi.
+  static double truncateRadiansAngle(double angle) {
+    if (angle < 0) {
+      angle = angle + (pi * 2);
+    } else if (angle > (pi * 2)) {
+      angle = angle - (pi * 2);
+    }
+    return angle;
+  }
+
+  /// Returns the index of the section that is being hovered.
+  static int determineHoverSection(
+      double total, double angle, List<CirculitoSection> sections) {
+    var startAngle = 0.0;
+
+    for (int i = 0; i < sections.length; i++) {
+      final section = sections[i];
+      final sectionAngle = (section.value / total) * 2 * pi;
+      final sectionEndAngle = startAngle + sectionAngle;
+
+      if (angle >= startAngle && (angle <= sectionEndAngle)) {
+        // Hovering this section.
+        return i;
+      }
+      startAngle += sectionAngle;
+    }
+
+    return -1;
   }
 }
