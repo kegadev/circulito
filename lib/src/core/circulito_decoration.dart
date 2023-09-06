2c0d18eac1f8bb0f6e45e8a8500e8d44e32674bb
kegadev <kegadev@outlook.com>
2023-09-06 12:54:37 -0500

    docs: update docs for decoration

--- lib/src/core/circulito_decoration.dart
+++ lib/src/core/circulito_decoration.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 
 import '../enums/enums.dart';
 
+/// The decoration of a section or background in the Circulito widget.
+///
+/// This decoration can contain either a solid `color` or a `gradient`. Users
+/// should instantiate it using one of the following constructors:
+/// - [CirculitoDecoration.fromColor] for a solid color.
+/// - [CirculitoDecoration.fromGradient] for a gradient.
+///
+/// Additionally, this decoration supports hover effects, which can be defined
+/// using `hoverColor` or `hoverGradient`, depending on the constructor used.
 class CirculitoDecoration {
   final Color? color;
   final DecorationType type;
@@ -9,11 +18,15 @@ class CirculitoDecoration {
   final Gradient? gradient;
   final Gradient? hoverGradient;
 
+  /// Creates a `decoration` from a solid color. If [hoverColor] is provided, the
+  /// section will change to that color when hovered.
   CirculitoDecoration.fromColor(this.color, {this.hoverColor})
       : type = DecorationType.color,
         gradient = null,
         hoverGradient = null;
 
+  /// Creates a `decoration` from a gradient. If [hoverGradient] is provided, the
+  /// section will change to that gradient when hovered.
   CirculitoDecoration.fromGradient(this.gradient, {this.hoverGradient})
       : type = DecorationType.gradient,
         color = null,
