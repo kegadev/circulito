e4149f38c0246d9392e22edf1b4d87dd5c3955a5
kegadev <kegadev@outlook.com>
2023-09-17 19:25:51 -0500

    chore: update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,8 +4,19 @@ import 'package:circulito/circulito.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Keys to prevent redraws on SetState calls.
+  final parentKey = GlobalKey();
+  final childKey = GlobalKey();
+
+  var _text = 'Circulito';
 
   @override
   Widget build(BuildContext context) {
@@ -17,50 +28,91 @@ class MyApp extends StatelessWidget {
             width: 500,
             height: 500,
             color: Colors.grey.shade200,
-            child: Circulito(
-              // Custom animation.
-              animation: CirculitoAnimation(
-                duration: 600,
-                curve: Curves.easeInOut,
-              ),
-              maxSize: 480,
-              strokeWidth: 80,
-              background: CirculitoBackground(),
-              strokeCap: CirculitoStrokeCap.round,
-              direction: CirculitoDirection.clockwise,
-              sectionValueType: SectionValueType.percentage,
-              sections: [
-                // Male percentage.
-                CirculitoSection(
-                  value: .35,
-                  decoration: const CirculitoDecoration.fromColor(
-                    Colors.blue,
-                    hoverColor: Colors.blueAccent,
-                  ),
-                  onHover: () {
-                    // Do something when mouse over this section.
-                  },
-                  onTap: () {
-                    // Do something when mouse tapped this section.
-                  },
+            child: SingleChildScrollView(
+              child: Circulito(
+                key: parentKey,
+                // Custom animation.
+                animation: CirculitoAnimation(
+                  duration: 600,
+                  curve: Curves.easeInOut,
                 ),
+                maxSize: 510,
+                strokeWidth: 50,
+                childStackingOrder: ChildStackingOrder.behindParent,
+                background: CirculitoBackground(),
+                strokeCap: CirculitoStrokeCap.butt,
+                direction: CirculitoDirection.clockwise,
+                sectionValueType: SectionValueType.percentage,
+                sections: [
+                  // Male percentage.
+                  CirculitoSection(
+                    value: .35,
+                    decoration: const CirculitoDecoration.fromColor(
+                      Colors.blue,
+                      hoverColor: Colors.blueAccent,
+                    ),
+                    onHover: () {
+                      setState(() => _text = 'Blue');
+                      // Do something when mouse over this section.
+                    },
+                    onTap: () {
+                      // Do something when mouse tapped this section.
+                    },
+                  ),
 
-                // Female percentage.
-                CirculitoSection(
-                  value: .40,
-                  decoration: const CirculitoDecoration.fromGradient(
-                    LinearGradient(colors: [Colors.pink, Colors.red]),
-                    hoverGradient: LinearGradient(colors: [
-                      Colors.pinkAccent,
-                      Colors.redAccent,
-                    ]),
+                  // Female percentage.
+                  CirculitoSection(
+                    value: .45,
+                    decoration: const CirculitoDecoration.fromGradient(
+                      LinearGradient(colors: [Colors.pink, Colors.red]),
+                      hoverGradient: LinearGradient(colors: [
+                        Colors.pinkAccent,
+                        Colors.redAccent,
+                      ]),
+                    ),
                   ),
-                ),
-              ],
+                ],
+                // Circulito as a child.
+                child: _getChild(),
+              ),
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Circulito _getChild() {
+    return Circulito(
+      key: childKey,
+      maxSize: 380,
+      strokeWidth: 50,
+      strokeCap: CirculitoStrokeCap.butt,
+      background: CirculitoBackground(),
+      startPoint: StartPoint.bottom,
+      animation: CirculitoAnimation(),
+      sectionValueType: SectionValueType.percentage,
+      sections: [
+        CirculitoSection(
+          value: .2,
+          onHover: () => setState(() => _text = 'Green'),
+          onTap: () => {
+            // Do something when mouse tapped this section.
+          },
+          decoration: const CirculitoDecoration.fromColor(Colors.green),
+        ),
+        CirculitoSection(
+          value: .3,
+          decoration: const CirculitoDecoration.fromColor(Colors.pink),
+        ),
+        CirculitoSection(
+          value: .2,
+          decoration: const CirculitoDecoration.fromColor(Colors.amber),
+        ),
+      ],
+
+      // Grandchild, to show text changes when sections are hovered.
+      child: Text(_text),
     );
   }
 }
