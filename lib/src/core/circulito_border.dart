93617dabe93723ddc84a62bc0e7b7d725c5b3681
kegadev <kegadev@outlook.com>
2023-09-29 20:45:34 -0500

    docs: update Circulito Border documentation

--- lib/src/core/circulito_border.dart
+++ lib/src/core/circulito_border.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+// The shadow to be painted behind the wheel or behind each section.
 class CirculitoBorder {
+  /// The color of the border.
   final Color color;
 
+  // The size of the border.
   final double size;
 
+  /// Creates a border to be painted around the wheel or around each section.
   const CirculitoBorder({
     this.color = Colors.black,
     this.size = 4.0,
