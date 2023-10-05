98793f32dd1f254264e0dacfe0f584b436e862a4
kegadev <kegadev@outlook.com>
2023-10-05 17:32:36 -0500

    refactor: genders example

--- example/lib/examples/genders.dart
+++ example/lib/examples/genders.dart
@@ -17,9 +17,9 @@ class _GendersState extends State<Genders> {
   static const _defaultText = 'Genders';
 
   final _circulitoKey = GlobalKey();
-  final _maxSize = 400.0;
-  final _malePercentage = 0.415;
   final _femalePercentage = 0.495;
+  final _malePercentage = 0.415;
+  final _maxSize = 400.0;
   final _padding = 40.0;
 
   var _text = _defaultText;
@@ -38,78 +38,84 @@ class _GendersState extends State<Genders> {
   }
 
   Circulito _genders(double maxSize) {
-    return Circulito(
-      key: _circulitoKey,
-      onHoverExit: () => setState(() => _text = _defaultText),
-      animation: CirculitoAnimation(
-        duration: 1000,
-        curve: Curves.easeInOut,
+    final animation = CirculitoAnimation(
+      duration: 1000,
+      curve: Curves.easeInOut,
+    );
+
+    final background = CirculitoBackground(
+      onHover: () => _changeChildText('Not defined.'),
+      decoration: const CirculitoDecoration.fromColor(
+        Colors.grey,
+        hoverColor: Colors.grey,
+        shadow: CirculitoShadow(),
       ),
+    );
+
+    final child = Text(
+      _text,
+      style: const TextStyle(fontSize: 24.0),
+    );
+
+    return Circulito(
       maxSize: maxSize,
       strokeWidth: 70.0,
-      childStackingOrder: ChildStackingOrder.behindParent,
-      background: CirculitoBackground(
-        onHover: () => setState(() => _text = 'Not defined.'),
-        decoration: const CirculitoDecoration.fromColor(
-          Colors.grey,
-          hoverColor: Colors.grey,
-          shadow: CirculitoShadow(),
-        ),
-      ),
+      key: _circulitoKey,
+      animation: animation,
+      background: background,
       strokeCap: CirculitoStrokeCap.butt,
       direction: CirculitoDirection.clockwise,
+      sections: [_maleSection(), _femaleSection()],
       sectionValueType: SectionValueType.percentage,
-      sections: [
-        // Male percentage.
-        CirculitoSection(
-          value: _malePercentage,
-          decoration: const CirculitoDecoration.fromGradient(
-            LinearGradient(colors: [
-              Color(0xFF17EAD9),
-              Color(0xFF6078EA),
-            ]),
-            hoverGradient: LinearGradient(colors: [
-              Colors.lightBlueAccent,
-              Colors.blueAccent,
-            ]),
-            // hoverColor: Colors.blueAccent,
-          ),
-          onHover: () {
-            setState(() => _text = 'Male');
+      onHoverExit: () => _changeChildText(_defaultText),
+      childStackingOrder: ChildStackingOrder.behindParent,
+      child: child,
+    );
+  }
 
-            // Do something when mouse over this section.
-          },
-          onTap: () {
-            // Default alert
-            setState(() {
-              _text = _text == "Male" ? "${_malePercentage * 100}%" : "Male";
-            });
-            // Do something when mouse tapped this section.
-          },
-        ),
+  /// Changes the text of the child widget.
+  void _changeChildText(String text) {
+    if (!mounted) return;
+    setState(() => _text = text);
+  }
 
-        // Female percentage.
-        CirculitoSection(
-          value: _femalePercentage,
-          onHover: () => setState(() => _text = "Female"),
-          onTap: () {
-            setState(() {
-              _text =
-                  _text == "Female" ? "${_femalePercentage * 100}%" : "Female";
-            });
-          },
-          decoration: const CirculitoDecoration.fromGradient(
-            LinearGradient(colors: [Color(0XFFF54EA2), Color(0xFFFF7676)]),
-            hoverGradient: LinearGradient(colors: [
-              Colors.pink,
-              Colors.pinkAccent,
-            ]),
-          ),
-        ),
-      ],
-      child: Text(
-        _text,
-        style: const TextStyle(fontSize: 24.0),
+  /// Switches the text of the child widget.
+  void _switchText(String defaultText, double percentage) {
+    if (!mounted) return;
+    _text = _text == defaultText ? "${percentage * 100}%" : defaultText;
+    setState(() {});
+  }
+
+  CirculitoSection _maleSection() {
+    return CirculitoSection(
+      value: _malePercentage,
+      onHover: () => _changeChildText('Male'),
+      onTap: () => _switchText("Male", _malePercentage),
+      decoration: const CirculitoDecoration.fromGradient(
+        LinearGradient(colors: [
+          Color(0xFF17EAD9),
+          Color(0xFF6078EA),
+        ]),
+        hoverGradient: LinearGradient(colors: [
+          Colors.lightBlueAccent,
+          Colors.blueAccent,
+        ]),
+        // hoverColor: Colors.blueAccent,
+      ),
+    );
+  }
+
+  CirculitoSection _femaleSection() {
+    return CirculitoSection(
+      value: _femalePercentage,
+      onHover: () => _changeChildText('Female'),
+      onTap: () => _switchText("Female", _femalePercentage),
+      decoration: const CirculitoDecoration.fromGradient(
+        LinearGradient(colors: [Color(0XFFF54EA2), Color(0xFFFF7676)]),
+        hoverGradient: LinearGradient(colors: [
+          Colors.pink,
+          Colors.pinkAccent,
+        ]),
       ),
     );
   }
